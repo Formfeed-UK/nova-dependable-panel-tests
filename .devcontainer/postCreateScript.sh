5a229b36446d139ee4c40ef91aa3efbb8df37e3b
ianrobertsFF <91917328+ianrobertsFF@users.noreply.github.com>
2022-10-23 22:37:50 +0000

    composer lock removal and nova install

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -10,10 +10,13 @@ cd /workspace
 cp .env.example .env
 git submodule init
 git submodule update
+rm -f composer.lock
 composer install
 php artisan key:generate
 php artisan storage:link
 php artisan dusk:install
+php artisan nova:install
+
 php artisan migrate
 yarn install
 yarn run development
